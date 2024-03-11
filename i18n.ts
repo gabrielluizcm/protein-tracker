344d564ce07b29c076210249e0ae75f680ab78cf
Gabriel Luiz <gabrielluizcm@hotmail.com>
2024-03-10 21:47:19 -0300

    First commit

---
+++ i18n.ts
@@ -0,0 +1,30 @@
+// i18n.js
+
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import translationEN from './locales/en.json';
+import translationPT from './locales/pt.json';
+
+// the translations
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  pt: {
+    translation: translationPT,
+  },
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: 'en',
+    keySeparator: false,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
