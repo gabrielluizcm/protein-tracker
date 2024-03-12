73c5ab81b2ba50707b1f56fe4f0bf16bc777a1e6
Gabriel Luiz <gabrielluizcm@hotmail.com>
2024-03-12 15:42:13 -0300

    Fix Tailwind issue on dynamic classes for radial graph

--- src/components/RadialProgress/index.tsx
+++ src/components/RadialProgress/index.tsx
@@ -1,3 +1,5 @@
+import arrTWPercentageStart from "./twPercentages";
+
 type RadialProgressProps = {
   type?: 'progress' | 'percentage';
   currentValue: number;
@@ -9,7 +11,7 @@ export default function RadialProgress({ type = 'percentage', currentValue, maxV
 
   return (
     <div className={`relative w-48 h-48 rounded-full text-gold 
-      bg-[conic-gradient(var(--tw-gradient-stops))] from-gold from-[${percentage}%] to-silver to-0%
+      bg-[conic-gradient(var(--tw-gradient-stops))] from-gold ${arrTWPercentageStart[currentValue]} to-silver to-[30%]
       flex items-center justify-center
       before:absolute before:w-32 before:h-32 before:rounded-full before:bg-black`}>
       <h3 className={`relative pointer-events-none ${type === 'progress' ? 'text-xl' : 'text-2xl'} font-bold`}>
