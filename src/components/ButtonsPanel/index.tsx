bc63177bddb671454135849e6b0bda61f3f383bb
Gabriel Luiz <gabrielluizcm@hotmail.com>
2024-05-15 12:18:44 -0300

    Fix buttons positioning on mobile

--- src/components/ButtonsPanel/index.tsx
+++ src/components/ButtonsPanel/index.tsx
@@ -6,14 +6,14 @@ type ButtonsPanelProps = {
 
 export default function ButtonsPanel({ changeCounter }: ButtonsPanelProps) {
   return (
-    <section className="flex w-1/2 items-center justify-center">
-      <div className="w-2/3 flex justify-center gap-5">
+    <section className="flex w-full md:w-1/2 items-center justify-center">
+      <div className="w-4/5 flex justify-center gap-2 md:gap-5">
         <CounterButtons value={-1} onClick={(value) => changeCounter(value)} />
         <CounterButtons value={-5} onClick={(value) => changeCounter(value)} />
         <CounterButtons value={-10} onClick={(value) => changeCounter(value)} />
       </div>
       <div className="w-[0px] h-full rounded-md border border-silver"></div>
-      <div className="w-2/3 flex justify-center gap-5">
+      <div className="w-4/5 flex justify-center gap-2 md:gap-5">
         <CounterButtons value={1} onClick={(value) => changeCounter(value)} />
         <CounterButtons value={5} onClick={(value) => changeCounter(value)} />
         <CounterButtons value={10} onClick={(value) => changeCounter(value)} />
