02a62e8be149cca5452c0a741d78e7208355ba7a
Gabriel Luiz <gabrielluizcm@hotmail.com>
2024-05-20 14:46:10 -0300

    Move CounterButtons click handler inside itself

--- src/components/ButtonsPanel/index.tsx
+++ src/components/ButtonsPanel/index.tsx
@@ -1,33 +1,18 @@
-import { useAcc, useNewAcc } from "../../hooks/contexts";
-
-import { setStorage } from "../../utils/storage";
-
 import CounterButtons from "../CounterButtons";
 
 export default function ButtonsPanel() {
-  const { state: currentCounter } = useAcc();
-  const { setState: setNewCounter } = useNewAcc();
-
-  const changeCounter = (value: number) => {
-    let newValue = currentCounter + value;
-    if (newValue < 0)
-      newValue = 0;
-    setNewCounter(newValue);
-    setStorage('currentValue', newValue.toString());
-  }
-
   return (
     <section className="flex w-full md:w-1/2 items-center justify-center">
       <div className="w-4/5 flex justify-center gap-2 md:gap-5">
-        <CounterButtons value={-1} onClick={(value) => changeCounter(value)} />
-        <CounterButtons value={-5} onClick={(value) => changeCounter(value)} />
-        <CounterButtons value={-10} onClick={(value) => changeCounter(value)} />
+        <CounterButtons value={-1} />
+        <CounterButtons value={-5} />
+        <CounterButtons value={-10} />
       </div>
       <div className="w-[0px] h-full rounded-md border border-silver"></div>
       <div className="w-4/5 flex justify-center gap-2 md:gap-5">
-        <CounterButtons value={1} onClick={(value) => changeCounter(value)} />
-        <CounterButtons value={5} onClick={(value) => changeCounter(value)} />
-        <CounterButtons value={10} onClick={(value) => changeCounter(value)} />
+        <CounterButtons value={1} />
+        <CounterButtons value={5} />
+        <CounterButtons value={10} />
       </div>
     </section>
   );
--- src/components/CounterButtons/index.tsx
+++ src/components/CounterButtons/index.tsx
@@ -1,13 +1,27 @@
+import { useAcc, useNewAcc } from "../../hooks/contexts";
+
+import { setStorage } from "../../utils/storage";
+
 type CounterButtonProps = {
   value: number;
-  onClick: (value: number) => void;
 }
 
-export default function CounterButtons({ value, onClick }: CounterButtonProps) {
+export default function CounterButtons({ value }: CounterButtonProps) {
   const add = value > 0;
 
+  const { state: currentCounter } = useAcc();
+  const { setState: setNewCounter } = useNewAcc();
+
+  const handleBtnClick = (value: number) => {
+    let newValue = currentCounter + value;
+    if (newValue < 0)
+      newValue = 0;
+    setNewCounter(newValue);
+    setStorage('currentValue', newValue.toString());
+  }
+
   return (
-    <button type="button" onClick={() => onClick(value)}
+    <button type="button" onClick={() => handleBtnClick(value)}
       className={`rounded-md p-5 w-8 h-8 ${add ? 'bg-gold' : 'bg-crimson'} text-black hover:text-silver 
         active:bg-copper flex items-center justify-center font-semibold text-xl`}>
       {add ? '+' : ''}{value}
